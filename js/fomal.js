440d6b4e940caecc87761583b8923f5e75d18f69
Zhongye1 <2760913192@qq.com>
2024-07-07 11:28:23 +0800

    Site updated: 2024-07-07 11:28:23

--- js/fomal.js
+++ js/fomal.js
@@ -3182,9 +3182,9 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://pic3.zhimg.com/v2-1e47d2da440013ae7d4262f54c26dd52_b.webp); 
+    --default-bg: url(https://pic4.zhimg.com/80/v2-1ad7934722e7c4ffc876ef97f1c1be13_1440w.webp); 
     --darkmode-bg:url(https://pic1.zhimg.com/v2-576643299b0497c2a341e4ab4b7b4030_b.webp);
-    --mobileday-bg: url(https://pic1.zhimg.com/v2-576643299b0497c2a341e4ab4b7b4030_b.webp);
+    --mobileday-bg: url(https://pic4.zhimg.com/80/v2-1ad7934722e7c4ffc876ef97f1c1be13_1440w.webp);
     --mobilenight-bg: url(https://pic1.zhimg.com/v2-576643299b0497c2a341e4ab4b7b4030_b.webp);
   }`; 
 }

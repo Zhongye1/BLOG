3366985655cd6650ee05a0906762f6a2fe6ece1c
Zhongye1 <2760913192@qq.com>
2024-08-31 16:27:37 +0800

    Site updated: 2024-08-31 16:27:36

--- ABM/config/config.js
+++ ABM/config/config.js
@@ -136,7 +136,7 @@ var config = {
     url: [
         //背景图、头像、作品展示背景、其他经历背景
         "./images/intro-bg.jpg",
-        "./images/2.jpg",
+        "./images/2.jpg",//"./images/2.jpg",
         "./images/work-bk.png",
         "./images/4.jpg"
     ]
